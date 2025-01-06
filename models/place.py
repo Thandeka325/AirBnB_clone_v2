994319cff956f50be55ef8c5b7adf4c252d7505f
Thandeka325 <marhwayiza325@gmail.com>
2025-01-05 18:24:07 -0800

    Project: 0x02. AirBnB clone - MySQL

--- models/place.py
+++ models/place.py
@@ -35,20 +35,16 @@ class Place(BaseModel, Base):
         amenity_ids: list of Amenity ids
     """
     __tablename__ = "places"
-    city_id = Column(
-            String(60), ForeignKey("cities.id"), nullable=False, default=""
-    )
-    user_id = Column(
-            String(60), ForeignKey("users.id"), nullable=False, default=""
-    )
-    name = Column(String(128), nullable=False, default="")
-    description = Column(String(1024), nullable=True, default=None)
+    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True, default=None)
-    longitude = Column(Float, nullable=True, default=None)
+    latitude = Column(Float)
+    longitude = Column(Float)
     amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
@@ -63,11 +59,17 @@ class Place(BaseModel, Base):
         def reviews(self):
             """ Returns list of reviews.id """
             var = models.storage.all()
-            return [
-                obj
-                for obj in var.values()
-                if isinstance(obj, Review) and obj.place_id == self.id
-            ]
+            lista = []
+            result = []
+            for key in var:
+                review = key.replace('.', ' ')
+                review = shlex.split(review)
+                if (review[0] == 'Review'):
+                    lista.append(var[key])
+            for elem in lista:
+                if (elem.place_id == self.id):
+                    result.append(elem)
+            return (result)
 
         @property
         def amenities(self):
