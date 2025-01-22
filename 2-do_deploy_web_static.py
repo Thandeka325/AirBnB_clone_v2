133e67a0dc8e2436e5043da664c2a2d7a18f4407
Thandeka325 <marhwayiza325@gmail.com>
2025-01-22 14:26:31 -0800

    Project: 0x03. AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,68 +2,50 @@
 """
 Fabric script to distribute an archive to web servers.
 """
-from fabric.api import env, put, run
-import os
+import os.path
+from fabric.api import env
+from fabric.api import put
+from fabric.api import run
 
 env.hosts = ["34.237.91.31", "18.234.169.141"]
 
 
 def do_deploy(archive_path):
-    """
-    Distributes an archive to the web servers.
+    """Distributes an archive to a web server.
 
     Args:
-        archive_path (str): The path to the archive file to be deployed.
-
+        archive_path (str): The path of the archive to distribute.
     Returns:
-        bool: True if all operations are successful, otherwise False.
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    if not os.path.isfile(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    try:
-        # Extract file name and base name without extension
-        file_name = os.path.basename(archive_path)
-        name_no_ext = file_name.split('.')[0]
-
-        # Define the destination paths
-        tmp_path = f"/tmp/{file_name}"
-        release_path = f"/data/web_static/releases/{name_no_ext}/"
-
-        # Upload the archive to the /tmp/ directory on the server
-        if put(archive_path, tmp_path).failed:
-            return False
-
-        # Create the release directory
-        if run(f"mkdir -p {release_path}").failed:
-            return False
-
-        # Uncompress the archive into the release directory
-        if run(f"tar -xzf {tmp_path} -C {release_path}").failed:
-            return False
-
-        # Remove the archive from the server
-        if run(f"rm {tmp_path}").failed:
-            return False
-
-        # Move content out of the web_static directory
-        if run(f"mv {release_path}web_static/* {release_path}").failed:
-            return False
-
-        # Remove the now-empty web_static directory
-        if run(f"rm -rf {release_path}web_static").failed:
-            return False
-
-        # Delete the current symbolic link
-        if run("rm -rf /data/web_static/current").failed:
-            return False
-
-        # Create a new symbolic link
-        if run(f"ln -s {release_path} /data/web_static/current").failed:
-            return False
-
-        print("New version deployed!")
-        return True
-    except Exception as e:
-        print(f"Error during deployment: {e}")
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
