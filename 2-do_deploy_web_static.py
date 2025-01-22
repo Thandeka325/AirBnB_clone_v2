bdb0c7abec1c094bdb1e31367b0889024747e2c1
Thandeka325 <marhwayiza325@gmail.com>
2025-01-22 14:18:28 -0800

    Project: 0x03. AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@ Fabric script to distribute an archive to web servers.
 from fabric.api import env, put, run
 import os
 
-env.hosts = ['34.237.91.31', '18.234.169.141']
+env.hosts = ["34.237.91.31", "18.234.169.141"]
 
 
 def do_deploy(archive_path):
