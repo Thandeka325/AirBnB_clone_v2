100d4f0866cdfc849bdbd4bb82f9b4e0b64371a8
Thandeka325 <marhwayiza325@gmail.com>
2025-01-24 06:58:47 -0800

    Project: 0x03. AirBnB clone - Deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,10 +11,10 @@ env.hosts = ['34.237.91.31', '18.234.169.141']
 
 
 def do_pack():
-    """generates a tgz archive"""
+    """Generates a .tgz archive from the contents of the web_static folder."""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
+        if not isdir("versions"):
             local("mkdir versions")
         file_name = "versions/web_static_{}.tgz".format(date)
         local("tar -cvzf {} web_static".format(file_name))
@@ -24,29 +24,51 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Distributes an archive to the web servers."""
+    if not exists(archive_path):
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
         release_path = "/data/web_static/releases/"
+
+        # Upload the archive to /tmp/
         put(archive_path, '/tmp/')
+        # Create release directory
         run('mkdir -p {}{}/'.format(release_path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, release_path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(release_path, no_ext))
+        # Extract archive contents
+        run(
+            'tar -xzf /tmp/{} -C {}{}/'.format(
+                file_name, release_path, no_ext
+            )
+        )
+        # Remove uploaded archive
+        run('rm /tmp/{}'.format(file_name))
+        # Move files from `web_static` directory
+        run(
+            'mv {0}{1}/web_static/* {0}{1}/ || true'.format(
+                release_path, no_ext
+            )
+        )
+        # Remove now-empty `web_static` directory
         run('rm -rf {}{}/web_static'.format(release_path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(
-            release_path, no_ext))
+        # Update symbolic link
+        run(
+            'ln -s {}{}/ /data/web_static/current'.format(
+                release_path, no_ext
+            )
+        )
+
+        # Validate deployment
+        run('test -f /data/web_static/current/my_index.html')
         return True
     except Exception as e:
+        print(f"Deployment error: {e}")
         return False
 
 
 def deploy():
-    """creates and distributes an archive to the web servers"""
+    """Creates and distributes an archive to the web servers."""
     archive_path = do_pack()
     if archive_path is None:
         return False
