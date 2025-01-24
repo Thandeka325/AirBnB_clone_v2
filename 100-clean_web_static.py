41f4c4479d26f9e6fe98abe016d094f88c688df1
Thandeka325 <marhwayiza325@gmail.com>
2025-01-24 07:41:30 -0800

    Project: 0x03. AirBnB clone - Deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -20,13 +20,13 @@ def do_clean(number=0):
     """
     number = 1 if int(number) == 0 else int(number)
 
-    archives_names = sorted(os.listdir("versions"))
+    archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives_names]
+        [local("rm ./{}".format(a)) for a in archives]
 
     with cd("/data/web_static/releases"):
-        archives_names = run("ls -tr").split()
-        archives_names = [a for a in archives if "web_static_" in a]
-        [archives_names.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives_names]
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
