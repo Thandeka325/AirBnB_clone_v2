bf224561fa80fa797c7aab1f7be3270a7216eb8e
Thandeka325 <marhwayiza325@gmail.com>
2025-02-08 09:48:02 -0800

    Task 8: List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,7 +3,6 @@
 
 from flask import Flask, render_template
 from models import *
-from models import storage
 
 
 app = Flask(__name__)
@@ -12,16 +11,16 @@ app = Flask(__name__)
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Displays a list of all State objects sorted by name"""
-    all_states = storage.all("State").values()
-    states = sorted(list(all_states), key=lambda state: state.name)
+    all_states = storage.all("State")
+    states = sorted(all_states.values(), key=lambda x: x.name)
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage on teardown"""
+def close_storage(exception):
+    """Handles teardown for SQLAlchemy session"""
     storage.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
