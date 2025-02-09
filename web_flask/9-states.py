2a7329611926681ba36cf33d030a7eeb16272aab
Thandeka325 <marhwayiza325@gmail.com>
2025-02-09 01:05:50 -0800

    Task 10: States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,38 +1,29 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-"""
+"""Flask web application to display states and their cities"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
 
 
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
-def states():
-    """Displays an HTML page with a list of all States.
-
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
-
-
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_db(exception):
+    """Remove the current SQLAlchemy session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_list(id=None):
+    """Displays a list of states or cities of a given state"""
+    states = storage.all(State)
+    if id:
+        state = states.get(f"State.{id}")
+        return render_template('9-states.html', state=state)
+    return render_template('9-states.html', states=states.values())
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
