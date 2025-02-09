455d78669dfcb4a3a85b01a27b15b085e139e189
Thandeka325 <marhwayiza325@gmail.com>
2025-02-09 00:35:42 -0800

    Task 9: Cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""Starts a Flask web application to display states and cities
-"""
+"""Starts a Flask web application to list States and Cities"""
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
+@app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Displays an HTML page listing all states and their cities.
-    """
-    states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    """Displays an HTML page with states and their cities"""
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_db(exception):
+    """Closes the database session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
